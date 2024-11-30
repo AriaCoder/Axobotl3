f2bd386269b8dc40edbf1b401e90ae46dfd553ef
vasitita8 <vasitita8@gmail.com>
2024-11-29 19:14:20 -0800

    Delete extra windCat

--- src/main.py
+++ src/main.py
@@ -201,14 +201,6 @@ def onBumperPressed():
     ledLeft.set_color(Color.GREEN)
     buttBumperPressed.broadcast()
 
-def releaseCat(cancelRewind = None): # Down Button
-    releaseHug()
-    catBeltRight.spin_for(FORWARD, 180, DEGREES, wait=False)
-    catBeltLeft.spin_for(FORWARD, 180, DEGREES)
-    # cancelWinding lets the caller of releaseCatapult() know
-    # if winding should be cancelled (keeps tension off rubber bands)
-    if (cancelRewind is None or not cancelRewind()): windCat()
-
 def windCat():  # Up Button
     releaseHug()
     catBeltLeft.spin(FORWARD)
@@ -221,6 +213,15 @@ def windCat():  # Up Button
     catBeltRight.spin_for(FORWARD, 10, DEGREES, wait = False)
     catBeltLeft.spin_for(FORWARD, 10, DEGREES)
     stopCatAndBelt()
+
+
+def releaseCat(cancelRewind = None): # Down Button
+    releaseHug()
+    catBeltRight.spin_for(FORWARD, 180, DEGREES, wait=False)
+    catBeltLeft.spin_for(FORWARD, 180, DEGREES)
+    # cancelWinding lets the caller of releaseCatapult() know
+    # if winding should be cancelled (keeps tension off rubber bands)
+    if (cancelRewind is None or not cancelRewind()): windCat()
 
 def releaseHug(stop: bool = True):
     if stop: stopCatAndBelt()
